f53c188247db2aa176011e4e438dfe132b0db750
Ben <bencoullie@gmail.com>
2014-04-10 21:35:21 -0800

    added albums page, fixed illu page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function(){
      $('.logoimg').delay(850).animate({
           opacity: 1
-     }, 2100)
+     }, 2100);
+     $('.logoimgillu').delay(250).animate({
+          opacity: 1
+     }, 1000);
+     $('.comingsoon').delay(1250).animate({
+          opacity: 1
+     }, 1000);
 });  
+
